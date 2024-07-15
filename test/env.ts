6d812dfb27ad765eb06e5eace60d07751acddbf4
LJ <3708366+worksofliam@users.noreply.github.com>
2024-07-15 15:27:59 -0400

    Update env.ts

--- test/env.ts
+++ test/env.ts
@@ -1,5 +1,8 @@
 import { DaemonServer } from "../src/types";
 
+console.log(creds);
+console.log(process.env);
+
 export const ENV_CREDS: DaemonServer = {
   host: process.env.VITE_SERVER || `localhost`,
   port: Number(process.env.VITE_PORT) || 8085,
