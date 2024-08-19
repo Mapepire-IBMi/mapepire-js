9bce906ab99c8e6e7fe14779d89225c2a31ebcbd
Jonathan Zak <jonnyzak32@gmail.com>
2024-08-19 09:52:37 -0400

    Add more pool tests

--- test/pool.test.ts
+++ test/pool.test.ts
@@ -81,31 +81,28 @@ test("Starting size of 0", async () => {
 });
 
 test("Performance test", async () => {
-  const startPool1 = Date.now();
   let pool = new Pool({ creds, maxSize: 5, startingSize: 5 });
   await pool.init();
+  const startPool1 = Date.now();
   let queries = [];
-
   for (let i = 0; i < 20; i++) {
     queries.push(pool.execute("select * FROM SAMPLE.SYSCOLUMNS"));
   }
-
   let results: QueryResult<any>[] = await Promise.all(queries);
   const endPool1 = Date.now();
   await pool.end();
   results.forEach((res) => expect(res.has_results).toBe(true));
 
-  const startPool2 = Date.now();
   pool = new Pool({ creds, maxSize: 1, startingSize: 1 });
   await pool.init();
+  const startPool2 = Date.now();
   queries = [];
-
   for (let i = 0; i < 20; i++) {
     queries.push(pool.execute("select * FROM SAMPLE.SYSCOLUMNS"));
   }
-
   results = await Promise.all(queries);
   const endPool2 = Date.now();
+
   await pool.end();
   results.forEach((res) => expect(res.has_results).toBe(true));
 
@@ -126,14 +123,16 @@ test("Pop jobs returns free job", async () => {
   let pool = new Pool({ creds, maxSize: 5, startingSize: 5 });
   await pool.init();
   expect(pool.getActiveJobCount()).toBe(5);
-  const res = await Promise.all([
+  const executedPromises = [
     pool.execute("select * FROM SAMPLE.SYSCOLUMNS"),
     pool.execute("select * FROM SAMPLE.SYSCOLUMNS"),
-    pool.popJob(),
-  ]);
-  const job = res[2] as SQLJob;
+  ];
+  const job = await pool.popJob();
   expect(job.getUniqueId()).toMatch(/sqljob/);
+  expect(job.getStatus()).toBe(JobStatus.Ready);
+  expect(job.getRunningCount()).toBe(0);
   expect(pool.getActiveJobCount()).toBe(4);
+  await Promise.all(executedPromises);
   await pool.end();
 });
 
@@ -141,16 +140,14 @@ test("Pop job with pool ignore", async () => {
   let pool = new Pool({ creds, maxSize: 1, startingSize: 1 });
   await pool.init();
   expect(pool.getActiveJobCount()).toBe(1);
-  const res = await Promise.all([
-    pool.execute("select * FROM SAMPLE.SYSCOLUMNS"),
-    pool.popJob(),
-  ]);
+  const executedPromises = [pool.execute("select * FROM SAMPLE.SYSCOLUMNS")];
   // Since all the jobs are busy, expect that a new job should be created.
   // Since no job was removed from the pool, and none was added, pool size shouldn't
   // change
-  const job: SQLJob = res[1];
+  const job = await pool.popJob();
   expect(job.getStatus()).toBe(JobStatus.Ready);
   expect(pool.getActiveJobCount()).toBe(1);
+  await Promise.all(executedPromises);
   await pool.end();
 });
 
@@ -159,11 +156,15 @@ test("Pool with no space, no ready job doesn't increase pool size", async () => 
   await pool.init();
   const addJobSpy = vi.spyOn(pool as any, "addJob");
   expect(pool.getActiveJobCount()).toBe(1);
-  await Promise.all([
+  const executedPromises = [
     pool.execute("select * FROM SAMPLE.SYSCOLUMNS"),
     pool.execute("select * FROM SAMPLE.SYSCOLUMNS"),
     pool.execute("select * FROM SAMPLE.SYSCOLUMNS"),
-  ]);
+  ];
+  const job = pool.getJob();
+  expect(job.getStatus()).toBe(JobStatus.Busy);
+  expect(job.getRunningCount()).toBe(3);
+  await Promise.all(executedPromises);
   expect(addJobSpy).not.toHaveBeenCalled();
   expect(pool.getActiveJobCount()).toBe(1);
   await pool.end();
@@ -174,12 +175,11 @@ test("Pool with no space but ready job returns ready job", async () => {
   await pool.init();
   expect(pool.getActiveJobCount()).toBe(2);
   const addJobSpy = vi.spyOn(pool as any, "addJob");
-  const res = await Promise.all([
-    pool.execute("select * FROM SAMPLE.SYSCOLUMNS"),
-    pool.getJob(),
-  ]);
-  const job: SQLJob = res[1];
+  const executedPromise = [pool.execute("select * FROM SAMPLE.SYSCOLUMNS")];
+  const job = pool.getJob();
   expect(job.getStatus()).toBe(JobStatus.Ready);
+  expect(job.getRunningCount()).toBe(0);
+  await Promise.all(executedPromise);
   expect(addJobSpy).not.toHaveBeenCalled();
   await pool.end();
 });
@@ -189,14 +189,42 @@ test("Pool with space but no ready job, adds job to pool", async () => {
   await pool.init();
   const addJobSpy = vi.spyOn(pool as any, "addJob");
   expect(pool.getActiveJobCount()).toBe(1);
-  await Promise.all([
+
+  // Initiate a bunch of jobs
+  const executedPromises = [
     pool.execute("select * FROM SAMPLE.SYSCOLUMNS"),
     pool.execute("select * FROM SAMPLE.SYSCOLUMNS"),
     pool.execute("select * FROM SAMPLE.SYSCOLUMNS"),
     pool.execute("select * FROM SAMPLE.SYSCOLUMNS"),
     pool.execute("select * FROM SAMPLE.SYSCOLUMNS"),
-    pool.getJob(),
-  ]);
+  ];
+  const job = pool.getJob();
+  expect(job.getStatus()).toBe(JobStatus.Busy);
+  expect(job.getRunningCount()).toBe(5);
+  await Promise.all(executedPromises);
+
   expect(addJobSpy).toHaveBeenCalled();
+  await pool.end();
+});
+
+test("Freeist job is returned", async () => {
+  const pool = new Pool({ creds, maxSize: 3, startingSize: 3 });
+  await pool.init();
+
+  // Initiate a bunch of jobs
+  const executedPromises = [
+    pool.execute("select * FROM SAMPLE.SYSCOLUMNS"),
+    pool.execute("select * FROM SAMPLE.SYSCOLUMNS"),
+    pool.execute("select * FROM SAMPLE.SYSCOLUMNS"),
+    pool.execute("select * FROM SAMPLE.SYSCOLUMNS"),
+    pool.execute("select * FROM SAMPLE.SYSCOLUMNS"),
+    pool.execute("select * FROM SAMPLE.SYSCOLUMNS"),
+    pool.execute("select * FROM SAMPLE.SYSCOLUMNS"),
+    pool.execute("select * FROM SAMPLE.SYSCOLUMNS"),
+  ];
+  const job = pool.getJob();
+  expect(job.getStatus()).toBe(JobStatus.Busy);
+  expect(job.getRunningCount()).toBe(2);
+  await Promise.all(executedPromises);
   await pool.end();
 });
