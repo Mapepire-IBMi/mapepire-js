88dca7f1bf10c4d0e0c2bf3579604fd0aa0dfe59
worksofliam <mrliamallan@live.co.uk>
2024-08-23 11:04:57 -0400

    Test cases for IN, INOUT and OUT parameters
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

--- test/procedures.test.ts
+++ test/procedures.test.ts
@@ -45,19 +45,138 @@ test(`IN, OUT, INOUT number parameters`, async () => {
   const queryA = job.query<any[]>(testProc);
   await queryA.execute();
   await queryA.close();
-  
-  job.enableLocalTrace();
 
   const queryB = job.query<any[]>(`call ${TEST_SCHEMA}.procedure_test(?, ?, ?)`, {parameters: [6, 4, 0]});
   const result = await queryB.execute();
   await queryB.close();
 
-  expect(result.is_done).toBe(true);
+  expect(result.metadata.parameters).toBeDefined();
+  const inParmNames = result.metadata.parameters.map(p => p.name);
+  const inParmTypes = result.metadata.parameters.map(p => p.type);
+  expect(inParmNames).toEqual(["P1", "P2", "P3"]);
+  expect(inParmTypes).toEqual(["INTEGER", "INTEGER", "INTEGER"]);
 
-  // TODO: check output values
+  expect(result.success).toBe(true);
+  expect(result.parameter_count).toBe(3);
+  expect(result.update_count).toBe(0);
+  expect(result.has_results).toBe(false);
+  expect(result.data.length).toBe(0);
+
+  expect(result.output_parms).toBeDefined();
+  expect(result.output_parms.length).toBe(3);
+  const outParmNames = result.output_parms.map(p => p.name);
+  const outParmTypes = result.output_parms.map(p => p.type);
+  const outParmValues = result.output_parms.map(p => p.value);
+
+  expect(outParmNames).toEqual(["P1", "P2", "P3"]);
+  expect(outParmTypes).toEqual(["INTEGER", "INTEGER", "INTEGER"]);
+  expect(outParmValues).toEqual([undefined, 0, 10]);
 
   await job.close();
 });
 
-// TODO: test case for char parmaeters
-// TODO: test case for varchar parmaeters
+test(`IN, OUT, INOUT char parameters`, async () => {
+  const job = new SQLJob();
+  await job.connect(creds);
+
+  const testProc = `
+    create or replace procedure ${TEST_SCHEMA}.procedure_test_char(
+      in p1 char(5),
+      inout p2 char(6),
+      out p3 char(7)
+    )
+    BEGIN
+      set p3 = rtrim(p1) concat rtrim(p2);
+      set p2 = '';
+    END
+  `;
+
+  const queryA = job.query<any[]>(testProc);
+  await queryA.execute();
+  await queryA.close();
+
+  const queryB = job.query<any[]>(`call ${TEST_SCHEMA}.procedure_test_char(?, ?, ?)`, {parameters: ['a', 'b', '']});
+  const result = await queryB.execute();
+  await queryB.close();
+
+  expect(result.metadata.parameters).toBeDefined();
+  const inParmNames = result.metadata.parameters.map(p => p.name);
+  const inParmTypes = result.metadata.parameters.map(p => p.type);
+  const inPrecisions = result.metadata.parameters.map(p => p.precision);
+  expect(inParmNames).toEqual(["P1", "P2", "P3"]);
+  expect(inParmTypes).toEqual(["CHAR", "CHAR", "CHAR"]);
+  expect(inPrecisions).toEqual([5, 6, 7]);
+
+  expect(result.success).toBe(true);
+  expect(result.parameter_count).toBe(3);
+  expect(result.update_count).toBe(0);
+  expect(result.has_results).toBe(false);
+  expect(result.data.length).toBe(0);
+
+  expect(result.output_parms).toBeDefined();
+  expect(result.output_parms.length).toBe(3);
+  const outParmNames = result.output_parms.map(p => p.name);
+  const outParmTypes = result.output_parms.map(p => p.type);
+  const outParmPrecisions = result.output_parms.map(p => p.precision);
+  const outParmValues = result.output_parms.map(p => p.value);
+
+  expect(outParmNames).toEqual(["P1", "P2", "P3"]);
+  expect(outParmTypes).toEqual(["CHAR", "CHAR", "CHAR"]);
+  expect(outParmPrecisions).toEqual([5, 6, 7]);
+  expect(outParmValues).toEqual([undefined, '', 'ab']);
+
+  await job.close();
+});
+
+test(`IN, OUT, INOUT varchar parameters`, async () => {
+  const job = new SQLJob();
+  await job.connect(creds);
+
+  const testProc = `
+    create or replace procedure ${TEST_SCHEMA}.procedure_test_varchar(
+      in p1 varchar(5),
+      inout p2 varchar(6),
+      out p3 varchar(7)
+    )
+    BEGIN
+      set p3 = p1 concat p2;
+      set p2 = '';
+    END
+  `;
+
+  const queryA = job.query<any[]>(testProc);
+  await queryA.execute();
+  await queryA.close();
+
+  const queryB = job.query<any[]>(`call ${TEST_SCHEMA}.procedure_test_varchar(?, ?, ?)`, {parameters: ['a', 'b', '']});
+  const result = await queryB.execute();
+  await queryB.close();
+
+  expect(result.metadata.parameters).toBeDefined();
+  const inParmNames = result.metadata.parameters.map(p => p.name);
+  const inParmTypes = result.metadata.parameters.map(p => p.type);
+  const inPrecisions = result.metadata.parameters.map(p => p.precision);
+  expect(inParmNames).toEqual(["P1", "P2", "P3"]);
+  expect(inParmTypes).toEqual(["VARCHAR", "VARCHAR", "VARCHAR"]);
+  expect(inPrecisions).toEqual([5, 6, 7]);
+
+  expect(result.success).toBe(true);
+  expect(result.parameter_count).toBe(3);
+  expect(result.update_count).toBe(0);
+  expect(result.has_results).toBe(false);
+  expect(result.data.length).toBe(0);
+
+  expect(result.output_parms).toBeDefined();
+  expect(result.output_parms.length).toBe(3);
+  const outParmNames = result.output_parms.map(p => p.name);
+  const outParmTypes = result.output_parms.map(p => p.type);
+  const outParmPrecisions = result.output_parms.map(p => p.precision);
+  const outParmValues = result.output_parms.map(p => p.value);
+
+  expect(outParmNames).toEqual(["P1", "P2", "P3"]);
+  expect(outParmTypes).toEqual(["VARCHAR", "VARCHAR", "VARCHAR"]);
+  expect(outParmPrecisions).toEqual([5, 6, 7]);
+  expect(outParmValues).toEqual([undefined, '', 'ab']);
+
+  await job.close();
+});
