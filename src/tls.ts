42a5b639354ae86a18b9116dc0454f1ab7802013
Jonathan Zak <jonnyzak32@gmail.com>
2024-09-11 11:38:53 -0400

    Fix grammar

--- src/tls.ts
+++ src/tls.ts
@@ -26,7 +26,7 @@ export function getCertificate(server: DaemonServer): Promise<tls.DetailedPeerCe
 
     socket.once('error', (err) => {
       if (err.reason === "sslv3 alert handshake failure"){
-        err.message += `. Please ensure server is being run with Java version >= 11.`
+        err.message += `. Please ensure the Mapepire server is being run with Java version >= 11.`
       }
       reject(err);
     });
