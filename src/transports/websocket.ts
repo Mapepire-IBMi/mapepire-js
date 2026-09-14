969dc990aee0e11922ab89d4a981cd55ec2babfe
Sanjula Ganepola <Sanjula.Ganepola@ibm.com>
2026-09-14 14:52:03 -0400

    Fix truncation of large numbers
    Signed-off-by: Sanjula Ganepola <Sanjula.Ganepola@ibm.com>

--- src/transports/websocket.ts
+++ src/transports/websocket.ts
@@ -52,9 +52,14 @@ export class WebSocketTransport extends BaseTransport {
       ws.on("message", (data: Buffer) => {
         const asString = data.toString();
         this.trace(asString);
-        
+
         try {
-          const response: ServerResponse = JSON.parse(asString);
+          const response: ServerResponse = JSON.parse(asString, ((key: any, value: string, context: any) => {
+            if (context && typeof value === 'number' && !Number.isSafeInteger(value)) {
+              return BigInt(context.source);
+            }
+            return value;
+          }) as any);
           this.emitResponse(response);
         } catch (e: any) {
           console.error(`Error parsing response: ` + e);
